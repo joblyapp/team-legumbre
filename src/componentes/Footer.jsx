ff2fa38881b13e23840bec11346f8c235a82b3ac
Julian Flores <julianfloresdev@gmail.com>
2023-01-19 14:07:12 -0300

    JF-05: fix footer image routing and add landing images cursor pointer

--- src/componentes/Footer.jsx
+++ src/componentes/Footer.jsx
@@ -25,7 +25,7 @@ export default function Footer () {
                 </div>
                 <div className="w-1/2 p-4 md:w-1/4">
                     <a href="https://www.argentina.gob.ar/superintendencia-de-seguros">
-                        <img className="w-64 mx-auto" src="../public/imagenes/LogoSSN.png" alt="SSN" />
+                        <img className="w-64 mx-auto" src="/imagenes/LogoSSN.png" alt="SSN" />
                     </a>
                 </div>
             </div>
