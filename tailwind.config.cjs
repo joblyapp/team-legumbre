f629f53080363e3f96905aaf04436db8462746a0
Julian Flores <julianfloresdev@gmail.com>
2023-01-23 18:32:01 -0300

    JF-05: add Loading Component

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -15,6 +15,21 @@ module.exports = {
       screens: {
         'desk': '1440px',
       },
+      animation: {
+        loaderBallGrow: 'loaderBallGrow var(--tw-animate-speed) ease infinite',
+        loaderTriangulate: ' loaderTriangulate var(--tw-animate-speed) ease infinite'
+      },
+      keyframes: {
+        loaderBallGrow: {
+          '0%, 100%': { transform: 'scale(1.5)' },
+          '20%, 70%': { transform: 'none' }
+        },
+        loaderTriangulate: {
+          '0%, 100%': { transform: 'none' },
+          '33.333%': { transform: 'translate(120%, 175%)' },
+          '66.666%': { transform: 'translate(-95%, 175%)' }
+        }
+      }
     },
   },
   plugins: [],
